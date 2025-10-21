dfe0d4727ab5d5f632eb56700587615485ebc6ee
hatim <hatim.touil0@gmail.com>
2025-10-21 12:51:08 +0100

    ntg

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,17 +8,25 @@ interface recordsFormat {
   mx: string | null;
   ns: string | null;
   soa: string | null;
+  txt: string | null;
 }
 
 function Header() {
   const [inputValue, setInputValue] = useState("");
   const [isVisible, setIsVisible] = useState(true);
   const [records, setRecords] = useState([]);
+  // const [a, setA] = useState([]);
+  // const [aaaa, setAaaa] = useState([]);
+  // const [cname, setCname] = useState([]);
+  // const [mx, setMx] = useState([]);
+  // const [ns, setNs] = useState([]);
+  // const [soa, setSoa] = useState([]);
+  // const [txt, setTxt] = useState([]);
 
   const searchBtn = useRef<HTMLButtonElement | null>(null);
 
   // For testing:
-  const recordColumns = ["domain", "a", "aaaa", "cname", "mx", "ns", "soa"];
+  const recordColumns = ["domain", "a", "aaaa", "cname", "mx", "ns", "soa", "txt"];
   const recs: recordsFormat[] = [
     {
       domain: "example.com",
@@ -28,6 +36,7 @@ function Header() {
       mx: "10 mail.example.com",
       ns: null,
       soa: "ns1.example.com",
+      txt: null,
     },
     {
       domain: "anotherexample.org",
@@ -37,6 +46,7 @@ function Header() {
       mx: "5 mail.another.org",
       ns: null,
       soa: "ns1.another.org",
+      txt: null,
     },
   ];
 
@@ -55,6 +65,8 @@ function Header() {
 
       const data = await response.json();
       setRecords(data);
+      // setA(records[1]);
+      console.log(records);
     } catch (err) {
       console.error("Failed to fetch DNS records:", err);
     }
@@ -111,34 +123,37 @@ function Header() {
           isVisible ? "hidden" : ""
         } overflow-x-auto border rounded-2xl`}
       >
-        <table className="bg-gray-700/70 text-white divide-y-4 divide-white/70">
+        <table className="bg-white/10 backdrop-blur-md text-white divide-y-4 divide-white/70">
           <thead>
             <tr>
-              <th className="px-6 py-3 text-left text-white text-3xl font-semibold">
+              <th className="px-6 py-3 text-left text-white text-xl font-semibold">
                 Domain Name
               </th>
-              <th className="px-6 py-3 text-left text-white text-3xl font-semibold">
+              <th className="px-6 py-3 text-left text-white text-xl font-semibold">
                 A record
               </th>
-              <th className="px-6 py-3 text-left text-white text-3xl font-semibold">
+              <th className="px-6 py-3 text-left text-white text-xl font-semibold">
                 AAAA record
               </th>
-              <th className="px-6 py-3 text-left text-white text-3xl font-semibold">
+              <th className="px-6 py-3 text-left text-white text-xl font-semibold">
                 CNAME
               </th>
-              <th className="px-6 py-3 text-left text-white text-3xl font-semibold">
+              <th className="px-6 py-3 text-left text-white text-xl font-semibold">
                 MX record
               </th>
-              <th className="px-6 py-3 text-left text-white text-3xl font-semibold">
+              <th className="px-6 py-3 text-left text-white text-xl font-semibold">
                 NS record
               </th>
-              <th className="px-6 py-3 text-left text-white text-3xl font-semibold">
+              <th className="px-6 py-3 text-left text-white text-xl font-semibold">
                 SAO record
+              </th>
+              <th className="px-6 py-3 text-left text-white text-xl font-semibold">
+                TXT
               </th>
             </tr>
           </thead>
           <tbody className="bg-white/10 divide-y divide-white">
-            {recs.map((rec, idx) => (
+            {/* {recs.map((rec, idx) => (
               <tr key={idx}>
                 {recordColumns.map((col) => (
                   <td
@@ -149,7 +164,17 @@ function Header() {
                   </td>
                 ))}
               </tr>
-            ))}
+            ))} */}
+            <tr>
+              {recordColumns.map((col) => (
+                <td
+                  key={col}
+                  className="px-6 py-3 text-center text-white text-xl"
+                >
+                  {records[0] ?? "-"}
+                </td>
+              ))}
+            </tr>
           </tbody>
         </table>
       </div>
